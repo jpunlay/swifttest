cf3af0ad588bdeafe7ccbf43189444bcfea41988
Jesus Punlay <30503097+jpunlay@users.noreply.github.com>
2021-10-26 22:14:11 -0400

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Jesus Punlay on 10/26/21.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
