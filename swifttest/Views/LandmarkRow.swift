be935c0adc97b3ffb8e2b361b037be3ce361d02b
Jesus Punlay <30503097+jpunlay@users.noreply.github.com>
2021-11-02 21:38:41 -0400

    restructure files

---
+++ swifttest/Views/LandmarkRow.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkRow.view.swift
+//  test
+//
+//  Created by Jesus Punlay on 11/2/21.
+//
+
+import SwiftUI
+
+struct LandmarkRow_view: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRow_view_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow_view()
+    }
+}
